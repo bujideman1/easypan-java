fe9fd933009cb85b7c4a1fbc23a2b2fbeff1d988
levir <530826534@qq.com>
2023-09-27 00:27:56 +0800

    注释9.26

--- easypan/src/main/java/com/easypan/entity/dto/UploadResultDto.java
+++ easypan/src/main/java/com/easypan/entity/dto/UploadResultDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 import java.io.Serializable;
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true)//反序列化的时候不报错
 public class UploadResultDto implements Serializable {
     private String fileId;
     private String status;
